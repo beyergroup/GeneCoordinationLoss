08198df2788b689661ea30764b3db042abcfe796
Ana Carolina Leote <anacarolinaleote@gmail.com>
2024-06-07 15:42:27 +0200

    Cleanup

--- Scripts/Clustering/run_tmp_totaleffects_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --account=adesous1
-#SBATCH --partition=single
-#SBATCH --ntasks=1
-#SBATCH --exclusive
-
-echo $threshold
-Rscript --vanilla tmp_totaleffects_test.R "/data/public/adesous1/GeneCorrelation/Outputs/Human_Network/stabsel/Networks/Adjacency_directed_weightsident_rownormalized_largest_cc.rds" ${threshold} "Outputs/Human_Network/stabsel/Networks/TotalEffects/"
